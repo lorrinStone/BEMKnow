7929df3bbe7567d853b817cc62547b784c13086e
lorrinStone <62714299+lorrinStone@users.noreply.github.com>
2021-11-24 21:57:18 -0500

    changed to forms, preparing java files [currently not functioning]

---
+++ phoneNumber.java
@@ -0,0 +1,15 @@
+package net.codejava.servlet;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet("/phoneNumber")
+public class LoginServlet extends HttpServlet {
+
+}
